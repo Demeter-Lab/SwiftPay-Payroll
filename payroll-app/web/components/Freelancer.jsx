33a18ee46af4cc5cffb5377e483985a31394b12a
midesofek <midesofek@gmail.com>
2023-10-03 21:47:26 +0100

    added test UI for project flow

---
+++ payroll-app/web/components/Freelancer.jsx
@@ -0,0 +1,13 @@
+export function Freelancer() {
+  console.log("Hello");
+  return (
+    <>
+      <main className="main">
+        <div className="cards">
+          <div>{"Single Payment"}</div>
+          <div>{"Batch Payment"}</div>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ payroll-app/web/pages/index.js
@@ -0,0 +1,78 @@
+import Head from "next/head";
+// import { Freelancer } from "@/components/Freelancer";
+import { Inter } from "next/font/google";
+import { useState } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  function SelectWhoToPay() {
+    const [isFreelancer, setIsFreelancer] = useState(false);
+    const [isEmployer, setIsEmployer] = useState(false);
+
+    function handleFreelancer() {
+      setIsEmployer(false);
+      setIsFreelancer(() => !isFreelancer);
+    }
+
+    function handleEmployer() {
+      setIsFreelancer(false);
+      setIsEmployer(() => !isEmployer);
+    }
+    return (
+      <>
+        <div className="cards">
+          <div onClick={handleFreelancer}>
+            <span>Freelancer</span>
+          </div>
+          <div onClick={handleEmployer}>
+            <span>Employer</span>
+          </div>
+        </div>
+        {isFreelancer ? <Freelancer /> : ""}
+        {isEmployer ? <Employer /> : ""}
+      </>
+    );
+  }
+
+  function Freelancer() {
+    console.log("Hello From Freelancer");
+    return (
+      <>
+        <div className="cards">
+          <div>{"Single Freelancer Payment "}</div>
+          <div>{"Batch Freelancer Payment"}</div>
+        </div>
+      </>
+    );
+  }
+
+  function Employer() {
+    console.log("Hello From Employer");
+    return (
+      <>
+        <div className="cards">
+          <div>{"Single Employer Payment"}</div>
+          <div>{"Batch Employer Payment"}</div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Payroll Dashboard</title>
+        <meta name="description" content="Built by Israel & Mide Sofek" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1> Payroll Dashboard</h1>
+      <main className="main">
+        <div>
+          <SelectWhoToPay />
+        </div>
+      </main>
+    </>
+  );
+}
