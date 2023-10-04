051e8353677d2870bca6a70a1848c90d3cd8d704
midesofek <midesofek@gmail.com>
2023-10-04 20:10:19 +0100

    Staging

---
+++ payroll-app/web/components/SelectWhoToPay.jsx
@@ -0,0 +1,125 @@
+import { useState } from "react";
+import { addWorker } from "@/flow-interactions/transactions";
+
+export function SelectWhoToPay() {
+  const [isFreelancer, setIsFreelancer] = useState(false);
+  const [isEmployer, setIsEmployer] = useState(false);
+
+  function handleFreelancer() {
+    setIsEmployer(false);
+    setIsFreelancer(() => !isFreelancer);
+  }
+
+  function handleEmployer() {
+    setIsFreelancer(false);
+    setIsEmployer(() => !isEmployer);
+  }
+
+  function Freelancer() {
+    console.log("Hello From Freelancer");
+    const [isSingleFreelancer, setIsSingleFreelancer] = useState(false);
+
+    function handleSingleFreelancer() {
+      setIsSingleFreelancer(() => !isSingleFreelancer);
+    }
+    return (
+      <>
+        <div className="cards">
+          <div onClick={handleSingleFreelancer}>
+            {"Single Freelancer Payment "}
+          </div>
+          <div>{"Batch Freelancer Payment"}</div>
+        </div>
+        {isSingleFreelancer ? <SingleFreelancer /> : ""}
+      </>
+    );
+  }
+
+  function Employer() {
+    console.log("Hello From Employer");
+    return (
+      <>
+        <div className="cards">
+          <div>{"Single Employer Payment"}</div>
+          <div>{"Batch Employer Payment"}</div>
+        </div>
+      </>
+    );
+  }
+
+  function SingleFreelancer() {
+    const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
+    const [totalPay, setTotalPay] = useState("");
+
+    // this function calls the addWorker function on the contract
+    async function callAddWorker() {
+      if ((name && address, totalPay)) {
+        // if successful alert
+        (await addWorker(name, address, totalPay)) &&
+          alert("Add Freelancer Successful");
+      } else {
+        alert("Field cannot be empty");
+      }
+    }
+
+    async function handleSubmit(e) {
+      try {
+        e.preventDefault();
+        // call the add worker function on flow
+        await callAddWorker();
+      } catch (err) {
+        alert(err.message);
+      }
+    }
+
+    return (
+      <>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <h1>Add Freelancer 👷‍♂️</h1>
+            <label>EnterName: </label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <br />
+
+            <label>Address: </label>
+            <input
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+            <br />
+
+            <label>Amount: </label>
+            <input
+              type="number"
+              value={totalPay}
+              onChange={(e) => setTotalPay(e.target.value)}
+            />
+
+            <button>Add</button>
+          </form>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="cards">
+        <div onClick={handleFreelancer}>
+          <span>Freelancer</span>
+        </div>
+        <div onClick={handleEmployer}>
+          <span>Employee</span>
+        </div>
+      </div>
+      {isFreelancer ? <Freelancer /> : ""}
+      {isEmployer ? <Employer /> : ""}
+    </>
+  );
+}
